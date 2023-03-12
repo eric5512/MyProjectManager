248a4b1dcf3367b1de8d7223696f5455ad63df8f
Eric <ericdandria2@gmail.com>
2023-03-12 05:32:00 +0100

    Update base.php

--- base.php
+++ base.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
     <script src="./bootstrap/js/bootstrap.min.js"></script>
-    <script src="./js.js"></script>
+    <script src="./jQuery.js"></script>
+    <script src="./jquery_ui/jquery-ui.js"></script>
+
     <title>My Project Manager</title>
     <style>
         body {
@@ -23,14 +25,14 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Project Manager</a>
+    <a class="navbar-brand" href="/index.php">Project Manager</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="/index.php?action=boards">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/index.php?action=calendar">Calendar</a>
@@ -42,7 +44,7 @@
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <?php 
               foreach ($boards as $name => $_) {
-                echo "<li><a class=\"dropdown-item\" href=\"#\">{$name}</a></li>";
+                echo "<li><a class=\"dropdown-item\" href=\"/index.php?action=board&board={$name}\">{$name}</a></li>";
               }
             ?>
           </ul>
@@ -51,3 +53,5 @@
     </div>
   </div>
 </nav>
+
+<br>
